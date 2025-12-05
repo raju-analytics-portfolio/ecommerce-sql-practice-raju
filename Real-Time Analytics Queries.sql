978820d7b37796c08434ac447904e7adf4568bc1
raju-analytics-portfolio <kunchalarajudatalab@gmail.com>
2025-12-05 22:43:39 +0530

    Pratices on real- time queries

--- Real-Time Analytics Queries.sql
+++ Real-Time Analytics Queries.sql
@@ -35,5 +35,101 @@ Kitchen	5
 Accessories	5
 Home Decor	3
 */
+# Number of orders for each payment mode
+select payment_mode, count(*) as total_payment
+from payments
+group by payment_mode;
+/* UPI	11
+Card	11
+Wallet	6
+COD	2
+*/
+# Number of orders placed each day
+select date(order_date) as orders_day, count(*) as total_orders
+from orders
+group by date(order_date);
+/* 2025-12-03	29
+2025-12-04	30
+*/
+# Highest payment amount
+select MAX(amount) as higher_amount
+from payments;
+/* 23999.00 */
+
+# Lowest payment amount
+select min(amount) as lowest_amount
+from payments;
+/* 120.00 */
+# List all payments that failed
+select * from Payments where payment_status="failed";
+/* 64	4	3200.00	UPI	Failed	2025-01-12 11:05:00
+68	8	799.00	Card	Failed	2025-01-15 08:10:00
+71	11	699.00	Card	Failed	2025-01-16 11:55:00
+78	18	999.00	Card	Failed	2025-01-19 11:50:00
+87	27	350.00	Card	Failed	2025-01-24 17:20:00
+*/
+# Count how many payments are pending
+select count(*) as pending_payments 
+from payments
+where payment_status="pending";
+/* 5 */
+
+# Orders that have no payment record
+SELECT o.order_id
+FROM orders o
+LEFT JOIN payments p ON o.order_id = p.order_id
+WHERE p.order_id IS NULL;
+
+# Number of customers by city
+SELECT city, COUNT(*) AS total_customers
+FROM customers
+GROUP BY city;
+
+# Count how many orders have been delivered
+SELECT COUNT(*) AS delivered_orders
+FROM delivery
+WHERE delivery_status = 'Delivered';
+
+# Count how many orders are “Out for Delivery”
+SELECT COUNT(*) AS out_for_delivery
+FROM delivery
+WHERE delivery_status = 'Out for Delivery';
+
+# Orders expected to be delivered today
+SELECT *
+FROM delivery
+WHERE expected_date = CURDATE();
+
+# Top 5 highest payment transactions
+SELECT *
+FROM payments
+ORDER BY amount DESC
+LIMIT 5;
+
+# Total number of products purchased per order
+SELECT order_id, SUM(quantity) AS total_products
+FROM order_items
+GROUP BY order_id;
+
+# Total COD orders
+SELECT COUNT(*) AS cod_orders
+FROM payments
+WHERE payment_mode = 'COD';
+
+# Orders delivered late (delivered_date > expected_date)
+SELECT *
+FROM delivery
+WHERE delivered_date > expected_date;
+
+# Average payment amount
+SELECT AVG(amount) AS average_payment
+FROM payments;
+
+# Orders not delivered yet
+SELECT *
+FROM delivery
+WHERE delivery_status <> 'Delivered';
+
+
 
 
